68d6f1549958622317b2edecc2848b1f9df9f15e
mack1ch <dimastepanov.1995@mail.ru>
2024-06-14 16:17:10 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,21 @@ import Bell from "../../public/icons/header/bell.svg";
 
 import { Main } from "@/entities/main-slice/main";
 import { Footer } from "@/entities/footer-slice/footer";
+import { useCookies } from "react-cookie";
+import { uid } from "uid";
+import { useEffect } from "react";
 
 export default function Home() {
+  const [cookies, setCookie] = useCookies(["user-id"]);
+  const isDevMode = process.env.NODE_ENV !== "production";
+  useEffect(() => {
+    if (cookies["user-id"]) {
+    } else {
+      setCookie("user-id", uid(16), {
+        secure: true,
+      });
+    }
+  }, []);
   return (
     <>
       <AppLayout>

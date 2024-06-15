972994fa9e3ea052f4ca0835298874dd4b1d78cb
mack1ch <dimastepanov.1995@mail.ru>
2024-06-15 20:56:50 +0500

    commit

--- src/entities/messanger-slice/message/model/index.tsx
+++ src/entities/messanger-slice/message/model/index.tsx
@@ -1,48 +1,63 @@
-import React from "react";
+import React, { ReactNode } from "react";
 
-export const formatText = (text: string[]) => {
-  const parts = [];
-  let temp = "";
+export const formatText = (texts: string[]): ReactNode[] => {
+  const parts: ReactNode[] = [];
+  texts.forEach((text, textIndex) => {
+    let temp: string = "";
+    let i: number = 0;
 
-  for (let i = 0; i < text.length; i++) {
-    if (text[i] === "*" && text[i + 1] === "*") {
-      // Bold text
-      if (temp) parts.push(temp);
-      temp = "";
-      let j = i + 2;
-      while (j < text.length && !(text[j] === "*" && text[j + 1] === "*")) {
-        temp += text[j];
-        j++;
+    while (i < text.length) {
+      if (text[i] === "*" && text[i + 1] === "*") {
+        // Bold text
+        if (temp) parts.push(temp);
+        temp = "";
+        i += 2; // Skip the opening **
+        while (i < text.length && !(text[i] === "*" && text[i + 1] === "*")) {
+          temp += text[i];
+          i++;
+        }
+        parts.push(
+          <span
+            key={`${textIndex}-${parts.length}`}
+            style={{ fontWeight: 600 }}
+          >
+            {temp}
+          </span>
+        );
+        temp = "";
+        i += 2; // Skip the closing **
+      } else if (text[i] === "`") {
+        // Italic text
+        if (temp) parts.push(temp);
+        temp = "";
+        i++; // Skip the opening `
+        while (i < text.length && text[i] !== "`") {
+          temp += text[i];
+          i++;
+        }
+        parts.push(
+          <span
+            key={`${textIndex}-${parts.length}`}
+            style={{ fontStyle: "italic" }}
+          >
+            {temp}
+          </span>
+        );
+        temp = "";
+        i++; // Skip the closing `
+      } else {
+        temp += text[i];
+        i++;
       }
-      parts.push(
-        <span key={parts.length} style={{ fontWeight: 600 }}>
-          {temp}
-        </span>
-      );
-      temp = "";
-      i = j + 1;
-    } else if (text[i] === "`") {
-      // Italic text
-      if (temp) parts.push(temp);
-      temp = "";
-      let j = i + 1;
-      while (j < text.length && text[j] !== "`") {
-        temp += text[j];
-        j++;
-      }
-      parts.push(
-        <span key={parts.length} style={{ fontStyle: "italic" }}>
-          {temp}
-        </span>
-      );
-      temp = "";
-      i = j;
-    } else {
-      temp += text[i];
     }
-  }
 
-  if (temp) parts.push(temp);
+    if (temp) parts.push(temp);
+
+    // Add a line break after each line except the last one
+    if (textIndex < texts.length - 1) {
+      parts.push(<br key={`br-${textIndex}`} />);
+    }
+  });
 
   return parts;
 };
